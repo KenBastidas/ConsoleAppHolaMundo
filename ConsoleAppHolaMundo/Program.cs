569d079b58eb711802e93d41b524d638df037eb4
BRIAN BASTIDAS <brian6bastidaz@gmail.com>
2020-11-05 12:56:12 -0500

    added suma of two numbres

---
+++ ConsoleAppHolaMundo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppHolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo C#");
+            Console.WriteLine("Integracopn de VS con GIT");
+            Console.ReadKey();
+        }
+    }
+}
